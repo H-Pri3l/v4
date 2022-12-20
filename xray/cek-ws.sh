c16bc7c17945033f90898f68fbe100000d87cec0
H-Pri3l <33812846+H-Pri3l@users.noreply.github.com>
2022-12-20 16:32:33 +0700

    Update cek-ws.sh

--- xray/cek-ws.sh
+++ xray/cek-ws.sh
@@ -13,9 +13,19 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl ipinfo.io/ip | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Fuck You"
+exit 0
+fi
 clear
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^####' | cut -d ' ' -f 2`);
+data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2`);
 echo "----------------------------------------";
 echo "---------=[ Vmess User Login ]=---------";
 echo "----------------------------------------";
@@ -52,5 +62,5 @@ oth=$(cat /tmp/other.txt | sort | uniq | nl)
 echo "other";
 echo "$oth";
 echo "----------------------------------------"
-echo "Script Mod By ADAM SIJA"
+echo "Script Mod By ZIXSTYLE"
 rm -rf /tmp/other.txt
