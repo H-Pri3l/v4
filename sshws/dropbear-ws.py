2959ed8f980cfa74ef08189fcd9c01792f2ee98d
H-Pri3l <33812846+H-Pri3l@users.noreply.github.com>
2023-01-15 18:21:08 +0700

    Update dropbear-ws.py

--- sshws/dropbear-ws.py
+++ sshws/dropbear-ws.py
@@ -2,7 +2,7 @@
 import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
-LISTENING_ADDR = '0.0.0.0'
+LISTENING_ADDR = '127.0.0.1'
 LISTENING_PORT = sys.argv[1]
 
 # Pass
@@ -12,7 +12,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:109'
-RESPONSE = 'HTTP/1.1 101 <b><font color="yellow"> YT ZIXSTYLE</font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 <b><font color="yellow"> YT ZIXSTYLE TUNNELING</font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
