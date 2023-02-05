33be453035821aa3df428a7aff9302920822bfe4
H-Pri3l <33812846+H-Pri3l@users.noreply.github.com>
2023-02-05 08:20:08 +0700

    Update xrayofficial.sh

--- xray/xrayofficial.sh
+++ xray/xrayofficial.sh
@@ -16,9 +16,21 @@ WB='\e[37;1m'
 echo ""
 echo -e "${YB}[ INFO ] Change Xray-core Official${NC}"
 # Install Xray-core Official
-rm -rf /etc/xray
-cp /backup/xray.official.backup /usr/local/bin/xray
-chmod 755 /usr/local/bin/xray
+mkdir /backup
+
+# / / Ambil Xray Core Version Terbaru
+latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+# / / Installation Xray Core
+xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
+# / / Ambil Xray Core Version Terbaru
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version $latest_version >/dev/null 2>&1
+
+rm -rf /usr/local/bin/xray
+cd `mktemp -d`
+curl -sL "$xraycore_link" -o xray.zip
+unzip -q xray.zip && rm -rf xray.zip
+mv xray /usr/local/bin/xray
+chmod +x /usr/local/bin/xray
 systemctl restart xray
 sleep 1
 echo -e "${YB}[ INFO ] Change Xray-core Official done${NC}"
