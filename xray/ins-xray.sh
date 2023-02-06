a16de51feda8c831334ccbf7f4d3b779de8f4403
H-Pri3l <33812846+H-Pri3l@users.noreply.github.com>
2023-02-07 01:10:44 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -538,13 +538,21 @@ cat >/etc/nginx/conf.d/xray.conf <<EOF
     server {
              listen 80;
              listen [::]:80;
-             listen 443 ssl http2 reuseport;
-             listen [::]:443 http2 reuseport;	
-             server_name $domain;
-             ssl_certificate /etc/xray/xray.crt;
-             ssl_certificate_key /etc/xray/xray.key;
-             ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
-             ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
+             listen 8880;
+             listen [::]:8880;
+             listen 55;
+             listen [::]:55;
+             listen 8080;                                                                                      
+             listen [::]:8080;                                                                                 
+             listen 8443 ssl http2 reuseport;                                                                  
+             listen [::]:8443 http2 reuseport;                                                                 
+             listen 2098 ssl ssl http2;                                                                        
+             listen [::]:2098 ssl http2;                                                                       
+             listen 443 ssl http2 reuseport;                                                                   
+             listen [::]:443 http2 reuseport;                                                                  
+             server_name zxt-p01.zixstyle.my.id;
+             ssl_certificate /etc/xray/xray.crt;                                                               ssl_certificate_key /etc/xray/xray.key;
+             ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128$             ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
              root /home/vps/public_html;
         }
 EOF
